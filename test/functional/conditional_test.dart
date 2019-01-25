9bfb7f8458a39059af44d022c8a9e67a6edee836
George Lesica <george@lesica.com>
2019-01-24 20:36:43 -0700

    Add conditional tests

--- test/functional/conditional_test.dart
+++ test/functional/conditional_test.dart
@@ -2,17 +2,13 @@ import 'package:dumblisp/dumblisp.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('math', () {
-    group('sum', () {
+  group('conditionals', () {
+    group('if', () {
       <String, Node>{
-        '(sum 0 1)': Int(1),
-        '(sum 1 1)': Int(2),
-        '(sum 0 1.0)': Float(1.0),
-        '(sum 1.0 1)': Float(2.0),
-        '(sum (sum 1 2) 3)': Int(6),
-        '(sum 3 (sum 1 2))': Int(6),
-        '(diff 1 0)': Int(1),
-        '(diff 1 1)': Int(0),
+        '(if true 1 0)': Int(1),
+        '(if false 1 0)': Int(0),
+        '((if true sum diff) 5 4)': Int(9),
+        '((if false sum diff) 5 4)': Int(1),
       }.forEach((source, expected) {
         test('"$source"', () {
           final tree = parseString(source).value;
